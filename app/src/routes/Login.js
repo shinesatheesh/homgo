af2e63e7a286eeedbdcc2be0df584fe48974ff31
satheesh <satheesh24798@gmail.com>
2024-01-01 18:45:26 +0530

    login input set

--- app/src/routes/Login.js
+++ app/src/routes/Login.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+import "./style/login.css";
 
-const Login = () => {
+function Login() {
+
+  const [email , setemail] = useState("");
+  const [pass , setpass] = useState("");
+  const handle = (event)=>{
+    if(event.target.name == "email"){
+      setemail(event.target.value);
+    }else{
+      setpass(event.target.value);
+    }
+  }
+  console.log(`email ${email}`)
+  console.log(`pass ${pass}`)
   return (
     <div>
-      <h1>login</h1>
+      {/* <Link to="/Register">Register</Link> */}
+      
+      <div>
+        <h1>login</h1>
+        <label>Email</label>
+        <input type='text' onChange={handle} 
+          value={email}
+          name='email' required /><br/>
+
+        <label>password</label>
+        <input type='password' onChange={handle} 
+          value={pass}
+          name="pass" required/><br/>
+        <button>sign in</button>
+      </div>
+      
     </div>
-  )
+  );
 }
 
 export default Login;
